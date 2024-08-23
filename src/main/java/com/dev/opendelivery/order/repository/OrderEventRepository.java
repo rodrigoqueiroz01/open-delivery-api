a0eb854eee2c3b3da95012690e943a2baaff336b
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2024-08-23 15:32:26 -0300

    refatoração

--- src/main/java/com/dev/opendelivery/order/repository/OrderEventRepository.java
+++ src/main/java/com/dev/opendelivery/order/repository/OrderEventRepository.java
@@ -3,7 +3,7 @@ package com.dev.opendelivery.order.repository;
 import com.dev.opendelivery.order.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Paramqqqq
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.*;
